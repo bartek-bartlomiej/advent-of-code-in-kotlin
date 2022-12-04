a63a4744161c905c7a318e74e2650fe812a2afae
Bartłomiej Stępień <35294021+bartek-bartlomiej@users.noreply.github.com>
2022-12-04 17:56:11 +0100

    [2022/01] Add another solution

--- src/main/kotlin/solutions/2022.01.kt
+++ src/main/kotlin/solutions/2022.01.kt
@@ -7,7 +7,15 @@ import strategies.ReadInputStrategy
 
 fun computePartOne(inventories: List<List<Int>>): Int = inventories.maxOf(List<Int>::sum)
 
-fun computePartTwo(inventories: List<List<Int>>): Int = inventories.map(List<Int>::sum).sorted().takeLast(3).sum()
+fun computePartTwo(inventories: List<List<Int>>): Int = getMaxThreeInLinearTime(inventories)
+
+fun getMaxThreeInNLogNTime(inventories: List<List<Int>>): Int =
+    inventories.map(List<Int>::sum).sorted().takeLast(3).sum()
+
+fun getMaxThreeInLinearTime(inventories: List<List<Int>>): Int =
+    inventories.fold(listOf(0, 0, 0)) { podium, inventory ->
+        (podium + inventory.sum()).sortedDescending().take(3)
+    }.sum()
 
 fun main() {
     Puzzle(
